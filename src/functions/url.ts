1f8d78938c1210b6dffe47a595d5aebce8f20f64
karthi <karthi@leaguex.com>
2022-06-16 16:49:18 +0530

    connected with dynamodb and data posted

--- src/functions/url.ts
+++ src/functions/url.ts
@@ -1,23 +1,42 @@
 import { APIGatewayProxyCallback, APIGatewayProxyEvent } from 'aws-lambda';
 import urlMetadata from 'url-metadata';
-export const handler = async (event:APIGatewayProxyEvent, context, callback:APIGatewayProxyCallback) => {
+import { uid } from 'uid';
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const AWS = require("aws-sdk");
+const dynamo = new AWS.DynamoDB.DocumentClient();
+
+export const handler = async (event: APIGatewayProxyEvent, context, callback: APIGatewayProxyCallback) => {
     try {
-      
+
         const reqBody = event?.body;
         const reqData = JSON.parse(reqBody);
-        const response={}
+        const response = {}
         if (reqData.url) {
-            // using url metadata
-           await urlMetadata(reqData?.url).then(
-                function (metadata) { // success handler
+                    // using url metadata
+            await urlMetadata(reqData?.url).then(
+                async function (metadata) { // success handler
                     // storing in response
-                    response["title"]=metadata?.['og:title']?metadata?.['og:title']:metadata?.title,
-                    response["description"]=metadata?.['og:description']?metadata?.['og:description']:metadata?.description
-                    response["keywords"]=metadata?.keywords
-                    response["images"]=metadata?.image
-                    response["type"]=metadata?.['og:type']
-                    response["ogImage"]=metadata?.['og:image']
+                    response["title"] = metadata?.['og:title'] ? metadata?.['og:title'] : metadata?.title,
+                        response["description"] = metadata?.['og:description'] ? metadata?.['og:description'] : metadata?.description
+                    response["keywords"] = metadata?.keywords
+                    response["images"] = metadata?.image
+                    response["type"] = metadata?.['og:type']
+                    response["ogImage"] = metadata?.['og:image']
 
+                    await dynamo
+                        .put({
+                            TableName: "metadata",
+                            Item: {
+                                id: uid(),
+                                url: reqData?.url,
+                                name: metadata?.['og:site_name'] ? metadata?.['og:site_name'] : "",
+                                title: metadata?.['og:title'] ? metadata?.['og:title'] : metadata?.title,
+                                description: metadata?.['og:description'] ? metadata?.['og:description'] : metadata?.description,
+                                images: metadata?.image,
+                            }
+                        })
+                        .promise();
                     // returning response
                     callback(null, {
                         statusCode: 200,
@@ -28,16 +47,17 @@ export const handler = async (event:APIGatewayProxyEvent, context, callback:APIG
                 function (error) { // failure handler
                     callback(error)
                 })
-        } else {
-            // handling no input error 
-            callback(null,{
-                statusCode: 400,
-                body: "Url Not given as input"
-            });
         }
-    } catch (e) {
-        console.log("error", e)
-        callback(e);
+     else {
+        // handling no input error 
+        callback(null, {
+            statusCode: 400,
+            body: "Url Not given as input"
+        });
     }
+} catch (e) {
+    console.log("error", e)
+    callback(e);
+}
 }
 
