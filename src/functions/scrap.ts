3a96b79bbe26458eab47a34d6fdc5084f9fb8be4
karthi <karthi@leaguex.com>
2022-06-16 20:52:15 +0530

    added inmem cache and added get and put in dynamo db

--- src/functions/scrap.ts
+++ src/functions/scrap.ts
@@ -2,88 +2,147 @@
 const request = require("request");
 import { APIGatewayProxyCallback, APIGatewayProxyEvent } from "aws-lambda";
 import * as cheerio from "cheerio";
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const AWS = require("aws-sdk");
+const dynamo = new AWS.DynamoDB.DocumentClient();
+import { uid } from 'uid';
 
-
-export const handler= (event:APIGatewayProxyEvent, context, callback:APIGatewayProxyCallback)=>{
+export const handler = async (event: APIGatewayProxyEvent, context, callback: APIGatewayProxyCallback) => {
     try {
-        const reqBody= event?.body
-        const parsedBody=JSON.parse(reqBody);
-       
-        if(parsedBody?.url) {
-            // calling promise to fetch html    
-            getHtml(parsedBody?.url).then(async (html:string)=>{
+        const reqBody = event?.body
+        const parsedBody = JSON.parse(reqBody);
+        //    try{
+        //     const userStatusParams = {
+        //         TableName: "metadata",
+        //         KeyConditionExpression: "#url = :url", 
+        //         ExpressionAttributeNames: {
+        //             "#url": "url",
+        //         },
+        //         ExpressionAttributeValues: {
+        //             ":url": JSON.stringify(parsedBody?.url),
+        //         },
+        //         FilterExpression: "#url = :url"
+        //       };
+        //       await dynamo.scan(userStatusParams, function (err, data){
+        //         if (err) console.log("err while getting ", err)
+        //         if (data) console.log("success", data)
+        //       })
+        //     } catch(e) {
+        //         console.log("err", e);
+        //         callback(e)
+        //     }
+        if (parsedBody?.url) {
+
+
+            await getHtml(parsedBody?.url).then(async (html: string) => {
                 // getting response from the scrapped data
-              const response=getScrappedData(html)
-              // returning response
-              callback(null, {
-                  statusCode: 200,
-                  body: JSON.stringify(response)
+                const response = await getScrappedData(parsedBody?.url, html)
+                // returning response
+                callback(null, {
+                    statusCode: 200,
+                    body: JSON.stringify(response)
                 })
-            }).catch((err)=>{
+            }).catch((err) => {
                 callback(err);
             })
-        } else {
-            callback(null, {
-                statusCode: 400,
-                body: "Input Not given"
-            });
-        }
-    } catch (e) {
-        callback(e);
+        
+
+
+        // calling promise to fetch html    
+
+    } else {
+        callback(null, {
+            statusCode: 400,
+            body: "Input Not given"
+        });
     }
+} catch (e) {
+    callback(e);
+}
 }
 
-export function getScrappedData(html:string) {
-    const response={}
+export async function getScrappedData(url: string, html: string) {
+    const response = {}
     const $ = cheerio.load(html),
-    //create a reference to the meta elements
-    $title = $('head title').text(),
-    $desc = $('meta[name="description"]').attr('content'),
-    $kwd = $('meta[name="keywords"]').attr('content'),
-    $ogTitle = $('meta[property="og:title"]').attr('content'),
-    $ogImage = $('meta[property="og:image"]').attr('content'),
-    $ogkeywords = $('meta[property="og:keywords"]').attr('content'),
-    $images = $('img');
+        //create a reference to the meta elements
+        $title = $('head title').text(),
+        $desc = $('meta[name="description"]').attr('content'),
+        $kwd = $('meta[name="keywords"]').attr('content'),
+        $ogTitle = $('meta[property="og:title"]').attr('content'),
+        $ogImage = $('meta[property="og:image"]').attr('content'),
+        $ogkeywords = $('meta[property="og:keywords"]').attr('content'),
+        $images = $('img');
 
-    if($title) {
-        response["title"]=$title;
+    if ($title) {
+        response["title"] = $title;
     }
-    if($desc) {
-        response["desc"]=$desc;
+    if ($desc) {
+        response["desc"] = $desc;
     }
-    if($ogTitle) {
-        response["description"]=$ogTitle;
+    if ($ogTitle) {
+        response["description"] = $ogTitle;
     }
-    if($kwd) {
-        response["kwd"]=$title;
+    if ($kwd) {
+        response["kwd"] = $title;
     }
-    if($ogkeywords) {
-        response["keywords"]=$ogkeywords
+    if ($ogkeywords) {
+        response["keywords"] = $ogkeywords
     }
-    if($images&&$images.length) {
-        response["images"]=[]
+    if ($images && $images.length) {
+        response["images"] = []
         for (let i = 0; i < $images.length; i++) {
             response["images"].push($($images[i]).attr('src'));
         }
     }
     if ($ogImage) {
-        response["ogImage"]=$ogImage
+        response["ogImage"] = $ogImage
     }
-    return response;
+    const userStatusParams = {
+        TableName: "metadata",
+        KeyConditionExpression: "#url = :url",
+        ExpressionAttributeNames: {
+            "#url": "url",
+        },
+        ExpressionAttributeValues: {
+            ":url": (url),
+        },
+        FilterExpression: "#url = :url"
+    };
+    console.log("url", url)
+    await dynamo.scan(userStatusParams,async function (err, data) {
+        if (err) console.log("err while getting ", err)
+        if (data)
+           console.log("success", data)
+     else {   
+    await dynamo
+        .put({
+            TableName: "metadata",
+            Item: {
+                id: uid(),
+                url: url,
+                title: $title,
+                description: $desc,
+                images: $ogImage ? $ogImage : "",
+            }
+        })
+        .promise();
+    }
+})
+return response;
 }
-export function getHtml(url:string) {
-    return new Promise ((resolve, reject)=>{
-        request(url,function (error:object, response:object, responseHtml:object) {
+export function getHtml(url: string) {
+    return new Promise((resolve, reject) => {
+        request(url, function (error: object, response: object, responseHtml: object) {
             //if there was an error
             if (error) {
                 reject(error)
             }
-           // responseHtml 
-           if (responseHtml) {
+            // responseHtml 
+            if (responseHtml) {
                 resolve(responseHtml)
-           } 
-           
-            
+            }
+
+
         })
     })
 }
--- src/functions/url.ts
+++ src/functions/url.ts
@@ -1,11 +1,11 @@
 import { APIGatewayProxyCallback, APIGatewayProxyEvent } from 'aws-lambda';
 import urlMetadata from 'url-metadata';
-import { uid } from 'uid';
+// import { uid } from 'uid';
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const AWS = require("aws-sdk");
-const dynamo = new AWS.DynamoDB.DocumentClient();
-
+// const AWS = require("aws-sdk");
+// const dynamo = new AWS.DynamoDB.DocumentClient();
+const inMemCache = new Map();
 export const handler = async (event: APIGatewayProxyEvent, context, callback: APIGatewayProxyCallback) => {
     try {
 
@@ -13,51 +13,60 @@ export const handler = async (event: APIGatewayProxyEvent, context, callback: AP
         const reqData = JSON.parse(reqBody);
         const response = {}
         if (reqData.url) {
-                    // using url metadata
-            await urlMetadata(reqData?.url).then(
-                async function (metadata) { // success handler
-                    // storing in response
-                    response["title"] = metadata?.['og:title'] ? metadata?.['og:title'] : metadata?.title,
-                        response["description"] = metadata?.['og:description'] ? metadata?.['og:description'] : metadata?.description
-                    response["keywords"] = metadata?.keywords
-                    response["images"] = metadata?.image
-                    response["type"] = metadata?.['og:type']
-                    response["ogImage"] = metadata?.['og:image']
-
-                    await dynamo
-                        .put({
-                            TableName: "metadata",
-                            Item: {
-                                id: uid(),
-                                url: reqData?.url,
-                                name: metadata?.['og:site_name'] ? metadata?.['og:site_name'] : "",
-                                title: metadata?.['og:title'] ? metadata?.['og:title'] : metadata?.title,
-                                description: metadata?.['og:description'] ? metadata?.['og:description'] : metadata?.description,
-                                images: metadata?.image,
-                            }
-                        })
-                        .promise();
-                    // returning response
-                    callback(null, {
-                        statusCode: 200,
-                        body: JSON.stringify(response)
-                    })
-                    // return
-                },
-                function (error) { // failure handler
-                    callback(error)
+           
+            if (inMemCache.has(reqData.url)) {
+                callback(null, {
+                    statusCode: 200,
+                    body: JSON.stringify(inMemCache.get(reqData.url))
                 })
+            } else {
+                // using url metadata
+                await urlMetadata(reqData?.url).then(
+                    async function (metadata) { // success handler
+                        // storing in response
+                        response["title"] = metadata?.['og:title'] ? metadata?.['og:title'] : metadata?.title,
+                            response["description"] = metadata?.['og:description'] ? metadata?.['og:description'] : metadata?.description
+                        response["keywords"] = metadata?.keywords
+                        response["images"] = metadata?.image
+                        response["type"] = metadata?.['og:type']
+                        response["ogImage"] = metadata?.['og:image']
+
+                        // await dynamo
+                        //     .put({
+                        //         TableName: "metadata",
+                        //         Item: {
+                        //             id: uid(),
+                        //             url: reqData?.url,
+                        //             name: metadata?.['og:site_name'] ? metadata?.['og:site_name'] : "",
+                        //             title: metadata?.['og:title'] ? metadata?.['og:title'] : metadata?.title,
+                        //             description: metadata?.['og:description'] ? metadata?.['og:description'] : metadata?.description,
+                        //             images: metadata?.image,
+                        //         }
+                        //     })
+                        //     .promise();
+                        inMemCache.set(reqData.url, response);
+                        // returning response
+                        callback(null, {
+                            statusCode: 200,
+                            body: JSON.stringify(response)
+                        })
+                        // return
+                    },
+                    function (error) { // failure handler
+                        callback(error)
+                    })
+            }
         }
-     else {
-        // handling no input error 
-        callback(null, {
-            statusCode: 400,
-            body: "Url Not given as input"
-        });
+        else {
+            // handling no input error 
+            callback(null, {
+                statusCode: 400,
+                body: "Url Not given as input"
+            });
+        }
+    } catch (e) {
+        console.log("error", e)
+        callback(e);
     }
-} catch (e) {
-    console.log("error", e)
-    callback(e);
-}
 }
 
